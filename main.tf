9b445d4b0485910d0dcbc053682cadb8742d62fd
Dmytro Fedoruk <softserve.dmytro@onemount.dev>
2023-05-31 14:27:54 +0300

    add localmudules

--- main.tf
+++ main.tf
@@ -10,8 +10,8 @@ locals {
 }
 
 module "ec2" {
-  #source            = "../../modules/e2esa-module-aws-ec2"
-  source             = "git::https://github.com/e2eSolutionArchitect/terraform.git//providers/aws/modules/e2esa-module-aws-ec2?ref=main"
+  source            = "modules/e2esa-module-aws-ec2"
+  #source             = "git::https://github.com/e2eSolutionArchitect/terraform.git//providers/aws/modules/e2esa-module-aws-ec2?ref=main"
   #for_each          = toset(var.ec2_names) # toset(["ec21","ec22"])
   ami               = var.ami
   availability_zone = var.availability_zone
